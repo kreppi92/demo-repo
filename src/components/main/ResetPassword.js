9272acd665b26878ce4bd8bbaaa520e9d14d62f1
Stefan Kovac <stefankovac416@gmail.com>
2020-04-12 02:20:01 -0400

    styles and consoles

--- src/components/main/ResetPassword.js
+++ src/components/main/ResetPassword.js
@@ -1,218 +1,233 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
-import { withStyles } from '@material-ui/core/styles'
-import { palette } from '../../constants/styles'
-import logoIcon from '../../images/logo.png'
-import Footer from './Footer'
-import Firebase from '../../constants/firebase'
-import CustomSnackbar from '../shared/CustomSnackbar'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import { palette } from "../../constants/styles";
+import logoIcon from "../../images/logo.png";
+import Footer from "./Footer";
+import Firebase from "../../constants/firebase";
+import CustomSnackbar from "../shared/CustomSnackbar";
 
-const queryString = require('query-string')
+const queryString = require("query-string");
 
 const styles = {
   button: {
-    margin: '30px 0 0 0'
+    margin: "30px 0 0 0",
   },
 
   viewLoader: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    height: '300px'
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    height: "300px",
   },
 
   viewCircularProgress: {
-    color: palette.blue[0]
+    color: palette.blue[0],
   },
 
   container: {
-    display: 'flex',
-    justifyContent: 'center'
+    display: "flex",
+    justifyContent: "center",
   },
 
   contentContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '40px 20px 40px 20px',
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "40px 20px 40px 20px",
 
-    '@media (min-width:780px)': {
-      padding: '40px 40px 40px 40px',
+    "@media (min-width:780px)": {
+      padding: "40px 40px 40px 40px",
     },
   },
 
   footer: {
-    margin: '95vh 0 0 0',
-    width: '100%'
+    margin: "95vh 0 0 0",
+    width: "100%",
   },
 
   paper: {
-    position: 'absolute',
-    width: '100%',
-    boxShadow: 'none',
-    borderRadius: '5px',
+    position: "absolute",
+    width: "100%",
+    boxShadow: "none",
+    borderRadius: "5px",
 
-    '@media (min-width:780px)': {
-      top: '20%',
-      width: '500px',
-      border: '1px solid',
-      borderColor: palette.gray[0]
+    "@media (min-width:780px)": {
+      top: "20%",
+      width: "500px",
+      border: "1px solid",
+      borderColor: palette.gray[0],
     },
   },
 
   linkContainer: {
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    margin: '25px 0 30px 0',
-    width: '100%'
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: "25px 0 30px 0",
+    width: "100%",
   },
 
   link: {
     color: palette.blue[0],
     fontWeight: 700,
-    fontSize: '15px',
-    textDecoration: 'none',
+    fontSize: "15px",
+    textDecoration: "none",
 
     "&:hover": {
-      textDecoration: 'underline'
-    }
+      textDecoration: "underline",
+    },
   },
 
   logoIcon: {
-    marginBottom: '30px',
-    objectFit: 'contain',
-    height: '40px',
-    width: '40px'
+    marginBottom: "30px",
+    objectFit: "contain",
+    height: "40px",
+    width: "40px",
   },
 
   textField: {
-    marginTop: '25px'
-  }
-}
+    marginTop: "25px",
+  },
+};
 
 class ResetPassword extends Component {
   state = {
-    password: '',
-    passwordHelperText: '',
+    password: "",
+    passwordHelperText: "",
     passwordError: false,
-    password2: '',
-    password2HelperText: '',
+    password2: "",
+    password2HelperText: "",
     password2Error: false,
     isLoading: false,
     isViewLoading: true,
     isFailedView: false,
     snackbarIsOpen: false,
-    snackbarVariant: 'success',
-    snackbarMessage: ''
-  }
+    snackbarVariant: "success",
+    snackbarMessage: "",
+  };
 
   componentDidMount() {
-    const parsed = queryString.parse(window.location.search)
-    console.log(parsed.reset)
+    const parsed = queryString.parse(window.location.search);
 
     if (parsed.reset === "") {
       this.setState({
         isViewLoading: false,
-        isFailedView: true
-      })
+        isFailedView: true,
+      });
     } else {
-      var isLinkValid = Firebase.functions().httpsCallable('isLinkValid')
-      isLinkValid({ token: parsed.reset, shouldReset: false }).then(function (result) {
-        console.log(result.data)
-        if (result.data.success) {
-          this.setState({
-            isViewLoading: false,
-            isFailedView: false
-          })
-        } else {
-          this.setState({
-            isViewLoading: false,
-            isFailedView: true
-          })
-        }
-      }.bind(this))
+      var isLinkValid = Firebase.functions().httpsCallable("isLinkValid");
+      isLinkValid({ token: parsed.reset, shouldReset: false }).then(
+        function (result) {
+          if (result.data.success) {
+            this.setState({
+              isViewLoading: false,
+              isFailedView: false,
+            });
+          } else {
+            this.setState({
+              isViewLoading: false,
+              isFailedView: true,
+            });
+          }
+        }.bind(this)
+      );
     }
   }
 
   onSnackBarClose = () => {
     this.setState({
-      snackbarIsOpen: false
-    })
-  }
+      snackbarIsOpen: false,
+    });
+  };
 
   displaySnackbar = (variant, message) => {
     this.setState({
       snackbarIsOpen: true,
       snackbarVariant: variant,
-      snackbarMessage: message
-    })
-  }
+      snackbarMessage: message,
+    });
+  };
 
-  handleChange = name => event => {
+  handleChange = (name) => (event) => {
     this.setState({
       [name]: event.target.value,
-      passwordHelperText: '',
+      passwordHelperText: "",
       passwordError: false,
-      password2HelperText: '',
-      password2Error: false
-    })
-  }
+      password2HelperText: "",
+      password2Error: false,
+    });
+  };
 
-  handleOnClick = event => {
-    this.validateForms()
-  }
+  handleOnClick = (event) => {
+    this.validateForms();
+  };
 
   validateForms() {
-    const { password, password2 } = this.state
+    const { password, password2 } = this.state;
 
-    var passwordHasError = false
-    var passwordErrorText = ""
+    var passwordHasError = false;
+    var passwordErrorText = "";
 
     if (password === "") {
-      passwordHasError = true
-      passwordErrorText = "Please enter a password."
+      passwordHasError = true;
+      passwordErrorText = "Please enter a password.";
     }
 
-    var password2HasError = false
-    var password2ErrorText = ""
+    var password2HasError = false;
+    var password2ErrorText = "";
 
     if (password2 === "") {
-      password2HasError = true
-      password2ErrorText = "Please confirm your password."
+      password2HasError = true;
+      password2ErrorText = "Please confirm your password.";
     }
 
     if (passwordHasError || password2HasError) {
-      this.displayFormError(passwordHasError, passwordErrorText, password2HasError, password2ErrorText)
+      this.displayFormError(
+        passwordHasError,
+        passwordErrorText,
+        password2HasError,
+        password2ErrorText
+      );
     } else {
       if (password === password2) {
-        this.updatePassword(password2)
+        this.updatePassword(password2);
       } else {
-        this.displayFormError(false, '', true, "Please ensure that both your password match.")
+        this.displayFormError(
+          false,
+          "",
+          true,
+          "Please ensure that both your password match."
+        );
       }
     }
   }
 
-  displayFormError(passwordHasError, passwordErrorText, password2HasError, password2ErrorText) {
+  displayFormError(
+    passwordHasError,
+    passwordErrorText,
+    password2HasError,
+    password2ErrorText
+  ) {
     this.setState({
       passwordError: passwordHasError,
       passwordHelperText: passwordErrorText,
       password2Error: password2HasError,
-      password2HelperText: password2ErrorText
-    })
+      password2HelperText: password2ErrorText,
+    });
   }
 
   updatePassword(newPassword) {
-    this.setState({ isLoading: true })
+    this.setState({ isLoading: true });
 
-    const parsed = queryString.parse(window.location.search)
+    const parsed = queryString.parse(window.location.search);
 
     if (parsed.reset === "") {
       this.setState({
@@ -220,171 +235,189 @@ class ResetPassword extends Component {
         password2: "",
         isViewLoading: false,
         isFailedView: true,
-        isLoading: false
-      })
+        isLoading: false,
+      });
     } else {
-      var isLinkValid = Firebase.functions().httpsCallable('isLinkValid')
-      isLinkValid({ token: parsed.reset, shouldReset: true }).then(function (result) {
-        if (result.data.success) {
-          this.resetPassword(newPassword, result.data.refreshToken)
-        } else {
-          this.setState({
-            isViewLoading: false,
-            isFailedView: false,
-            isLoading: false
-          })
+      var isLinkValid = Firebase.functions().httpsCallable("isLinkValid");
+      isLinkValid({ token: parsed.reset, shouldReset: true }).then(
+        function (result) {
+          if (result.data.success) {
+            this.resetPassword(newPassword, result.data.refreshToken);
+          } else {
+            this.setState({
+              isViewLoading: false,
+              isFailedView: false,
+              isLoading: false,
+            });
 
-          this.displaySnackbar('error', result.data.error)
-        }
-      }.bind(this))
+            this.displaySnackbar("error", result.data.error);
+          }
+        }.bind(this)
+      );
     }
   }
 
   resetPassword(newPassword, token) {
-    var updatePassword = Firebase.functions().httpsCallable('updatePassword')
-      updatePassword({ password: newPassword, token: token }).then(function (result) {
-        console.log(result.data)
+    var updatePassword = Firebase.functions().httpsCallable("updatePassword");
+    updatePassword({ password: newPassword, token: token }).then(
+      function (result) {
         if (result.data.success) {
           this.setState({
             password: "",
             password2: "",
             isViewLoading: true,
             isFailedView: false,
-            isLoading: false
-          })
+            isLoading: false,
+          });
 
-          this.displaySnackbar('success', 'Your password has successfully been updated. Transferring to sign in ...')
+          this.displaySnackbar(
+            "success",
+            "Your password has successfully been updated. Transferring to sign in ..."
+          );
 
           setTimeout(
             function () {
-              window.location = '/signin'
+              window.location = "/signin";
             }.bind(this),
-            2000,
-          )
+            2000
+          );
         } else {
           this.setState({
             isViewLoading: false,
             isFailedView: false,
-            isLoading: false
-          })
+            isLoading: false,
+          });
 
-          this.displaySnackbar('error', result.data.error)
+          this.displaySnackbar("error", result.data.error);
         }
-      }.bind(this))
+      }.bind(this)
+    );
   }
 
   render() {
-    const { classes } = this.props
-    const { password, passwordHelperText, passwordError, password2, password2HelperText, password2Error, isLoading, isFailedView, isViewLoading, snackbarIsOpen, snackbarMessage, snackbarVariant  } = this.state
+    const { classes } = this.props;
+    const {
+      password,
+      passwordHelperText,
+      passwordError,
+      password2,
+      password2HelperText,
+      password2Error,
+      isLoading,
+      isFailedView,
+      isViewLoading,
+      snackbarIsOpen,
+      snackbarMessage,
+      snackbarVariant,
+    } = this.state;
 
     return (
       <div className={classes.container}>
         <Paper className={classes.paper}>
-          {
-            isViewLoading ?
+          {isViewLoading ? (
+            <div className={classes.viewLoader}>
+              <CircularProgress
+                className={classes.viewCircularProgress}
+                variant="indeterminate"
+                disableShrink
+                size={24}
+                thickness={4}
+              />
+            </div>
+          ) : isFailedView ? (
+            <div className={classes.contentContainer}>
+              <img src={logoIcon} className={classes.logoIcon} alt="" />
 
-              <div className={classes.viewLoader}>
-                <CircularProgress
-                  className={classes.viewCircularProgress}
-                  variant="indeterminate"
-                  disableShrink
-                  size={24}
-                  thickness={4}
-                />
+              <Typography variant="h4" gutterBottom>
+                This link has expired and is in valid.
+              </Typography>
+
+              <div className={classes.linkContainer}>
+                <a className={classes.link} href={"/forgot_password"}>
+                  Forgot password?
+                </a>
+                <a className={classes.link} href={"/signin"}>
+                  Sign in
+                </a>
               </div>
+            </div>
+          ) : (
+            <div className={classes.contentContainer}>
+              <img src={logoIcon} className={classes.logoIcon} alt="" />
 
-              :
+              <Typography variant="h4" gutterBottom>
+                Enter your new password
+              </Typography>
 
-              isFailedView ?
+              <TextField
+                onKeyPress={this.onKeyPress}
+                fullWidth
+                error={passwordError}
+                className={classes.textField}
+                id="outlined-password"
+                label="Enter Password"
+                name="password"
+                type="password"
+                value={password}
+                helperText={passwordHelperText}
+                onChange={this.handleChange("password")}
+                variant="outlined"
+              />
 
-                <div className={classes.contentContainer}>
-                  <img src={logoIcon} className={classes.logoIcon} alt="" />
+              <TextField
+                onKeyPress={this.onKeyPress}
+                fullWidth
+                error={password2Error}
+                className={classes.textField}
+                id="outlined-password2"
+                label="Confirm Password"
+                name="password2"
+                type="password"
+                value={password2}
+                helperText={password2HelperText}
+                onChange={this.handleChange("password2")}
+                variant="outlined"
+              />
 
-                  <Typography variant="h4" gutterBottom>
-                    This link has expired and is in valid.
-                </Typography>
-
-                  <div className={classes.linkContainer}>
-                    <a className={classes.link} href={"/forgot_password"}>Forgot password?</a>
-                    <a className={classes.link} href={"/signin"}>Sign in</a>
-                  </div>
-
-                </div>
-
-                :
-
-                <div className={classes.contentContainer}>
-                  <img src={logoIcon} className={classes.logoIcon} alt="" />
-
-                  <Typography variant="h4" gutterBottom>
-                    Enter your new password
-                </Typography>
-
-                  <TextField
-                    onKeyPress={this.onKeyPress}
-                    fullWidth
-                    error={passwordError}
-                    className={classes.textField}
-                    id="outlined-password"
-                    label="Enter Password"
-                    name="password"
-                    type="password"
-                    value={password}
-                    helperText={passwordHelperText}
-                    onChange={this.handleChange('password')}
-                    variant="outlined"
+              <Button
+                className={classes.button}
+                size="large"
+                variant={"contained"}
+                color="primary"
+                fullWidth
+                onClick={this.handleOnClick}
+              >
+                {isLoading ? (
+                  <CircularProgress
+                    variant="indeterminate"
+                    disableShrink
+                    size={24}
+                    thickness={4}
                   />
-
-                  <TextField
-                    onKeyPress={this.onKeyPress}
-                    fullWidth
-                    error={password2Error}
-                    className={classes.textField}
-                    id="outlined-password2"
-                    label="Confirm Password"
-                    name="password2"
-                    type="password"
-                    value={password2}
-                    helperText={password2HelperText}
-                    onChange={this.handleChange('password2')}
-                    variant="outlined"
-                  />
-
-                  <Button
-                    className={classes.button}
-                    size="large"
-                    variant={'contained'}
-                    color="primary"
-                    fullWidth
-                    onClick={this.handleOnClick}
-                  >
-                    {isLoading ? (
-                      <CircularProgress
-                        variant="indeterminate"
-                        disableShrink
-                        size={24}
-                        thickness={4}
-                      />
-                    ) : (
-                        'Reset password'
-                      )}
-                  </Button>
-                </div>
-          }
-
+                ) : (
+                  "Reset password"
+                )}
+              </Button>
+            </div>
+          )}
         </Paper>
         <div className={classes.footer}>
           <Footer />
         </div>
 
-        <CustomSnackbar variant={snackbarVariant} message={snackbarMessage} open={snackbarIsOpen} onSnackBarClose={this.onSnackBarClose}/>
+        <CustomSnackbar
+          variant={snackbarVariant}
+          message={snackbarMessage}
+          open={snackbarIsOpen}
+          onSnackBarClose={this.onSnackBarClose}
+        />
       </div>
-    )
+    );
   }
 }
 
 ResetPassword.propTypes = {
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
-export default withStyles(styles)(ResetPassword)
+export default withStyles(styles)(ResetPassword);
